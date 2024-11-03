b669825a96d8ec49719923897561afe7537b736c
sortizaccess <sortiz@accessinformatica.com.ar>
2024-11-03 18:55:35 -0300

    Update index.ts

--- src/app/shared/utils/index.ts
+++ src/app/shared/utils/index.ts
@@ -4,8 +4,8 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 export function generarIdRandom(): string {
   const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let id = '';
-  
-  for (let i = 0; i < 5; i++) { 
+
+  for (let i = 0; i < 6; i++) { 
       id += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
   }
 
