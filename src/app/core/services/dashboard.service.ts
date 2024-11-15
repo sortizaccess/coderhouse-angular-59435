addd06e3c60febaa736cb7cc5cc9b54d783c4662
sortizaccess <sortiz@accessinformatica.com.ar>
2024-11-15 19:44:48 -0300

    Update dashboard.service.ts

--- src/app/core/services/dashboard.service.ts
+++ src/app/core/services/dashboard.service.ts
@@ -4,18 +4,19 @@ import { Usuario } from '../../core/models/usuario';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
+import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DashboardService {
   private _authUsuario$ = new BehaviorSubject<null | Usuario>(null);
   public authUsuario$ = this._authUsuario$.asObservable();
   private baseURL = environment.apiBaseURL;
-  constructor(private router: Router, private httpClient: HttpClient){}
+  constructor(private router: Router, private httpClient: HttpClient, private authService: AuthService){}
 
   validarToken(): Observable<boolean>{
     return this.httpClient.get<Usuario[]>(`${this.baseURL}/usuarios?token=${localStorage.getItem('token')}`)
     .pipe(map((usuarios) => {
-      const usuario = this.setToken(usuarios);
+      const usuario = this.authService.autentificarToken(usuarios);
       return !usuario;
     }));
   }
@@ -23,19 +24,10 @@ export class DashboardService {
   esAdmin(): Observable<boolean>{
     return this.httpClient.get<Usuario[]>(`${this.baseURL}/usuarios?token=${localStorage.getItem('token')}`)
     .pipe(map((usuarios) => {
-      const usuario = this.setToken(usuarios);
+      const usuario = this.authService.autentificarToken(usuarios);
       return !usuario?.esAdmin;
     }));
   }
 
-  setToken(usuarios: Usuario[]): Usuario | null {
-    if(usuarios[0]){
-      localStorage.setItem('token', usuarios[0].token);
-      this._authUsuario$.next(usuarios[0]);
-      return usuarios[0];
-    }
-    else {
-      return null;
-    }
-  }
+
 }
